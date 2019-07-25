37bd2e540c4aa163cea9b783ca39381455cb6c10
Seth Davison <seth@davison.net>
2019-07-25 11:44:50 -0400

    Remove unused imports

--- PitchTracker/src/PitchTrackerDriver.java
+++ PitchTracker/src/PitchTrackerDriver.java
@@ -5,20 +5,7 @@
 //-----------------------------------
 import java.util.Scanner;
 import java.io.*;
-import java.time.LocalDate;
-import java.awt.Color; 
-import java.awt.BasicStroke; 
-import org.jfree.chart.ChartPanel; 
-import org.jfree.chart.JFreeChart; 
-import org.jfree.data.xy.XYDataset; 
-import org.jfree.data.xy.XYSeries; 
-import org.jfree.ui.ApplicationFrame; 
-import org.jfree.ui.RefineryUtilities; 
-import org.jfree.chart.plot.XYPlot; 
-import org.jfree.chart.ChartFactory; 
-import org.jfree.chart.plot.PlotOrientation; 
-import org.jfree.data.xy.XYSeriesCollection; 
-import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.ui.RefineryUtilities;
 import javax.swing.JFrame;
 
 public class PitchTrackerDriver{
