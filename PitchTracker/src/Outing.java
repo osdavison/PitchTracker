4c2972fd0aeb8764f300b920ef659cc29ce8bc06
Seth Davison <seth@davison.net>
2019-07-25 11:16:45 -0400

    Calculate pitch type percentages

--- PitchTracker/src/Outing.java
+++ PitchTracker/src/Outing.java
@@ -85,16 +85,15 @@ public class Outing implements Serializable{
    public int getPitchCount(){   return this.pitches.size();   }
    
    public double[] getPercentages(){
-	      calcAll();
-	      double[] array = new double[6];
-	      array[0] = 70;
-	      array[1] = 20;
-	      array[2] = 22;
-	      array[3] = 44;
-	      array[4] = 54;
-	      array[5] = 0;
-	      return array;
-	   }
+      calcAll();
+      double[] array = new double[6];
+      
+      for(int i = 0; i < this.pitches.size(); i++) {
+    	  array[this.pitches.get(i).getType()-1] += 1;
+      }
+      
+      return array;
+   }
    
    //SETTERS---------------------------------------------------------------
    public void setPitcher(Pitcher p){this.pitcher = p;}
